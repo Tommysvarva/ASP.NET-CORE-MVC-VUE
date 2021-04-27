b43b0989bc71ad3eafac1a7973ad90ad9c165b78
Tommy <44683197+Tommysvarva@users.noreply.github.com>
2021-04-27 22:29:56 +0200

    Initial commit

---
+++ ASP.NET-CORE-MVC-VUE/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using ASP.NET_CORE_MVC_VUE.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_CORE_MVC_VUE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult VueOnly()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.NET-CORE-MVC-VUE/wwwroot/vue/VueOnly.js
@@ -0,0 +1,14 @@
+﻿var mvc_and_vue_app = new Vue({
+    el: '#app',
+    data: {
+        model: {
+            title: "vue only test!",
+            count: 0
+        }
+    },
+    methods: {
+        increment() {
+            this.model.count ++;
+        }
+    }
+})
